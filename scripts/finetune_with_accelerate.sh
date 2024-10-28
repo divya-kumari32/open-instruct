3337885caebebed59d930f7eec1c867eef8e2626
divykum2 <divykum2@ibm.com>
2024-10-28 13:03:03 -0400

    Added train_file and output_dir args

--- scripts/finetune_with_accelerate.sh
+++ scripts/finetune_with_accelerate.sh
@@ -1,10 +1,22 @@
+
+if [ "$#" -lt 2 ]; then
+  echo "Usage: $0 <train_file> <output_dir>"
+  exit 1
+fi
+
 export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
 
 NUM_GPUS=8
 BATCH_SIZE_PER_GPU=1
 TOTAL_BATCH_SIZE=128
 GRADIENT_ACC_STEPS=$(($TOTAL_BATCH_SIZE/$NUM_GPUS/$BATCH_SIZE_PER_GPU))
+
+TRAIN_FILE=$1
+OUTPUT_DIR=$2
+
 echo "Training Mamba model using $NUM_GPUS GPUs, $BATCH_SIZE_PER_GPU batch size per GPU, $GRADIENT_ACC_STEPS gradient accumulation steps"
+echo "Training file: ${TRAIN_FILE}"
+echo "Output directory: ${OUTPUT_DIR}"
 
 # You can also set --gradient_checkpointing or use `stage3_offloading_accelerate.conf` to save memory,
 # but it will trade off speed.
@@ -18,7 +30,7 @@ accelerate launch \
     --model_name_or_path /gpfs/users/lchu/ckpt/mamba2_9b_dolma_2t/hf/step_1280000_ckp \
     --use_flash_attn \
     --tokenizer_name /gpfs/users/lchu/ckpt/mamba2_9b_dolma_2t/hf/step_1280000_ckp \
-    --train_file data/processed/tulu_v2/daring_anteater_data.jsonl \
+    --train_file data/processed/"${TRAIN_FILE}" \
     --max_seq_length 4096 \
     --preprocessing_num_workers 128 \
     --per_device_train_batch_size $BATCH_SIZE_PER_GPU \
@@ -28,7 +40,7 @@ accelerate launch \
     --warmup_ratio 0.03 \
     --weight_decay 0. \
     --num_train_epochs 2 \
-    --output_dir output/mamba2_9b_daring_anteater_fine_tuned \
+    --output_dir output/"${OUTPUT_DIR}" \
     --report_to tensorboard \
     --logging_steps 1 \
     --try_launch_beaker_eval_jobs False \

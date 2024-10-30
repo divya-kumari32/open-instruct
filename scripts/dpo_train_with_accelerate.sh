e6a6c91ac923b4eaf7003c1648f9e7a44dd00049
divykum2 <divykum2@ibm.com>
2024-10-30 16:11:15 -0400

    Added model name and output dir args to dpo file

--- scripts/dpo_train_with_accelerate.sh
+++ scripts/dpo_train_with_accelerate.sh
@@ -1,3 +1,8 @@
+if [ "$#" -lt 2 ]; then
+  echo "Usage: $0 <train_file> <output_dir>"
+  exit 1
+fi
+
 # you need 8 GPUs for full finetuning
 export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
 
@@ -5,7 +10,13 @@ NUM_GPUS=8
 BATCH_SIZE_PER_GPU=1
 TOTAL_BATCH_SIZE=32
 GRADIENT_ACC_STEPS=$(($TOTAL_BATCH_SIZE/$NUM_GPUS/$BATCH_SIZE_PER_GPU))
+
+MODEL_NAME=$1
+OUTPUT_DIR=$2
+
 echo "Training model using $NUM_GPUS GPUs, $BATCH_SIZE_PER_GPU batch size per GPU, $GRADIENT_ACC_STEPS gradient accumulation steps"
+echo "Model: ${TRAIN_FILE}"
+echo "Output directory: ${OUTPUT_DIR}"
 
 accelerate launch \
     --mixed_precision bf16 \
@@ -14,9 +25,9 @@ accelerate launch \
     --use_deepspeed \
     --deepspeed_config_file configs/ds_configs/stage3_no_offloading_accelerate.conf \
     open_instruct/dpo_tune.py \
-    --model_name_or_path /gpfs/users/lchu/open-instruct/output/mamba2_9b_lr2e5_2epochs \
+    --model_name_or_path "${MODEL_NAME}" \
     --use_flash_attn \
-    --tokenizer_name /gpfs/users/lchu/open-instruct/output/mamba2_9b_lr2e5_2epochs \
+    --tokenizer_name "${MODEL_NAME}" \
     --use_slow_tokenizer \
     --dataset_name HuggingFaceH4/ultrafeedback_binarized \
     --max_seq_length 2048 \
@@ -28,7 +39,7 @@ accelerate launch \
     --warmup_ratio 0.1 \
     --weight_decay 0. \
     --num_train_epochs 3 \
-    --output_dir output/dpo_mamba \
+    --output_dir /gpfs/lchu/ckpt/mamba2_9b_dolma_2t/open_instruct/dpo/"${OUTPUT_DIR}" \
     --report_to tensorboard \
     --logging_steps 1 \
     --try_launch_beaker_eval_jobs False \

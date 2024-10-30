db3c521fa1f47955d34bd728a372d30cfe1aa245
divykum2 <divykum2@ibm.com>
2024-10-30 16:39:08 -0400

    Added model name and output dir args to dpo file

--- scripts/dpo_train_with_accelerate.sh
+++ scripts/dpo_train_with_accelerate.sh
@@ -15,7 +15,7 @@ MODEL_NAME=$1
 OUTPUT_DIR=$2
 
 echo "Training model using $NUM_GPUS GPUs, $BATCH_SIZE_PER_GPU batch size per GPU, $GRADIENT_ACC_STEPS gradient accumulation steps"
-echo "Model: ${TRAIN_FILE}"
+echo "Model: ${MODEL_NAME}"
 echo "Output directory: ${OUTPUT_DIR}"
 
 accelerate launch \
@@ -25,7 +25,7 @@ accelerate launch \
     --use_deepspeed \
     --deepspeed_config_file configs/ds_configs/stage3_no_offloading_accelerate.conf \
     open_instruct/dpo_tune.py \
-    --model_name_or_path "${MODEL_NAME}" \
+    --model_name_or_path /gpfs/lchu/ckpt/mamba2_9b_dolma_2t/open_instruct/"${MODEL_NAME}" \
     --use_flash_attn \
     --tokenizer_name /gpfs/lchu/ckpt/mamba2_9b_dolma_2t/open_instruct/"${MODEL_NAME}" \
     --use_slow_tokenizer \
